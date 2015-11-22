a6de9c076f95946c9b561616c5789411b061efbf
Steven Lindberg <stevenjohnlindberg@gmail.com>
2015-11-22 13:06:20 -0800

    Simplified tensor rotation util

--- app/utils/rotate-tensor.js
+++ app/utils/rotate-tensor.js
@@ -1,21 +1,7 @@
-import { dot } from 'numeric'
-import { identityMatrix } from '../constants/math'
+import { dot, transpose } from 'numeric'
 
-// Convert a tensor in one coordinate system to another
+// Rotate a tensor from its current coordinate system to another
+// See: http://www.continuummechanics.org/cm/stressxforms.html
 export default function transformTensor(tensor, rotation) {
-  // Create rotated unit axes
-  const normals = identityMatrix.map((axis) => {
-    return dot(rotation, axis)
-  })
-
-  // The elements of the transformed tensor are the product of tensor with the
-  // corresponding plane normal (row), then dotted with the corresponding unit
-  // normal (column)
-  return tensor.map((row, i) => {
-    return row.map((value, j) => {
-      // Note: 'dotting' a matrix with a vector is just a normal multiply (that
-      // results in a vector)
-      return dot(dot(tensor, normals[j]), normals[i])
-    })
-  })
+  return dot(dot(transpose(rotation), tensor), rotation)
 }
